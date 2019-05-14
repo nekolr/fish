e57f15e09899e49e5a0fc13c588d2c6dafb0c33b
nekolr <excalibll@163.com>
2019-05-14 14:11:51 +0800

    🔧 Update cache config

--- src/main/java/com/nekolr/fish/cache/redis/config/RedisConfig.java
+++ src/main/java/com/nekolr/fish/cache/redis/config/RedisConfig.java
@@ -4,11 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -16,7 +23,8 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 /**
  * Redis 配置类
@@ -25,7 +33,15 @@ import java.time.Duration;
  */
 @Configuration
 @EnableCaching
+@EnableConfigurationProperties(CacheProperties.class)
+@AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisConfig {
+
+    /**
+     * 使用配置文件
+     */
+    @Autowired
+    private CacheProperties cacheProperties;
 
     /**
      * 自定义缓存 key 生成策略
@@ -47,8 +63,21 @@ public class RedisConfig {
         };
     }
 
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration());
+
+        List<String> cacheNames = this.cacheProperties.getCacheNames();
+        if (!cacheNames.isEmpty()) {
+            builder.initialCacheNames(new LinkedHashSet<>(cacheNames));
+        }
+        return builder.build();
+    }
+
     /**
-     * 缓存配置，默认缓存数据过期时间为一天
+     * 缓存配置
      *
      * @return
      */
@@ -57,14 +86,24 @@ public class RedisConfig {
         // 生成一个默认的缓存配置
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
 
-        // 缓存过期时间为 1 天
-        configuration.entryTtl(Duration.ofDays(1))
+        // 缓存过期时间
+        configuration = configuration.entryTtl(cacheProperties.getRedis().getTimeToLive())
                 // 设置 key 值序列化
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(stringRedisSerializer()))
                 // 设置 value 值序列化
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer()))
-                // 不缓存空值
-                .disableCachingNullValues();
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer()));
+        // 是否允许缓存空值
+        if (!cacheProperties.getRedis().isCacheNullValues()) {
+            configuration = configuration.disableCachingNullValues();
+        }
+        // 如果配置中存在前缀则添加
+        if (StringUtils.isNotBlank(cacheProperties.getRedis().getKeyPrefix())) {
+            configuration = configuration.prefixKeysWith(cacheProperties.getRedis().getKeyPrefix());
+        }
+        // 是否使用前缀
+        if (!cacheProperties.getRedis().isUseKeyPrefix()) {
+            configuration = configuration.disableKeyPrefix();
+        }
 
         return configuration;
     }
@@ -76,13 +115,15 @@ public class RedisConfig {
      * @return
      */
     @Bean
-    public RedisTemplate<String, Object> redisTemplateBean(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         // 使用 StringRedisSerializer 序列化 redis 的 key
         redisTemplate.setKeySerializer(stringRedisSerializer());
         // 使用 Jackson2JsonRedisSerializer 序列化 redis 的 value
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(stringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer());
 
         redisTemplate.afterPropertiesSet();
 
@@ -98,7 +139,6 @@ public class RedisConfig {
     @Bean(name = "jackson2JsonRedisSerializer")
     public RedisSerializer<Object> jackson2JsonRedisSerializer() {
         Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
-
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
